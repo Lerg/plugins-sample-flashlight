cab3d2d94ac892c9ea9dc2babb19355909930e4c
Lerg <lvs.tourist@gmail.com>
2016-03-07 08:06:39 +0500

    Plugin no longer returns success boolean on Android due to Android 6 permission model.

--- main.lua
+++ main.lua
@@ -8,14 +8,10 @@ flashlight.on()
 
 Turn on the flashlight.
 
-Returns boolean, true on success.
-
 
 flashlight.off()
 
 Turn off the flashlight.
-
-Returns boolean, true on success.
 
 ]]
 
@@ -24,19 +20,12 @@ display.setDefault('background', 1)
 local x, y = display.contentCenterX, display.contentCenterY
 local w, h = display.contentWidth * 0.8, 50
 
-local function noFlash()
-    native.showAlert('Flashlight', 'Flash is not available.', {'OK'})
-end
-
 widget.newButton{
     x = x, y = y - 50,
     width = w, height = h,
     label = 'Flashlight ON',
     onRelease = function()
-        local success = flashlight.on()
-        if not success then
-            noFlash()
-        end
+        flashlight.on()
     end}
 
 widget.newButton{
@@ -44,8 +33,5 @@ widget.newButton{
     width = w, height = h,
     label = 'Flashlight OFF',
     onRelease = function()
-        local success = flashlight.off()
-        if not success then
-            noFlash()
-        end
+        flashlight.off()
     end}
